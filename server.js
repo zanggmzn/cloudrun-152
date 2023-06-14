bf85f0b287e290febca86215e682e4380a73e1b0
maijang <72776902+zanggmzn@users.noreply.github.com>
2023-06-14 17:20:44 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require('http')
+const PORT = process.env.PORT || 9000
+http
+  .createServer((req, res) => {
+    console.log('New connection')
+    res.end('Hello Cloud Run')
+  })
+  .listen(PORT, () => console.log('Listening on', PORT))
