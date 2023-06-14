f511f89b1dca77724501f67ff5c5a37a52cd4747
maijang <72776902+zanggmzn@users.noreply.github.com>
2023-06-14 18:09:58 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const PORT = process.env.PORT || 9000
 http
   .createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello Cloud Run')
+    res.end('Bismillah TCC A')
   })
   .listen(PORT, () => console.log('Listening on', PORT))
